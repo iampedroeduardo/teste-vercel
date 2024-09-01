f5507a75061a6c899d6475161c035a0764787474
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-09-01 20:29:08 -0300

    teste

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -2,6 +2,9 @@ import { sql } from '@vercel/postgres';
 
 async function teste(request,response){
     var { rows } = await sql `SELECT * FROM teste;`;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     response.json(rows);
 }
 export default teste;
